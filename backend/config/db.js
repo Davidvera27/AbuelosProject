1e55afa93eb586d5809e9a7aecaaf0606a147338
Davidvera27 <davidrestrepovera@gmail.com>
2024-10-20 21:44:38 -0500

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+// backend/config/db.js
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Salir del proceso si falla la conexión
+  }
+};
+module.exports = connectDB;
