87b15614142766526ad2b8635a77c1c969b3c0dc
Davidvera27 <davidrestrepovera@gmail.com>
2024-10-20 21:45:30 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,18 @@
-import React, { useState } from 'react';
-import './assets/css/App.css'; // El CSS que controla los estilos
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Registro from './components/Registro';
 
-function App() {
-  const [isLoginActive, setLoginActive] = useState(true); // Maneja el estado entre Login y Registro
-
-  const toggleLogin = () => {
-    setLoginActive(true);  // Activa el formulario de Login
-  };
-
-  const toggleRegister = () => {
-    setLoginActive(false); // Activa el formulario de Registro
-  };
-
+const App = () => {
   return (
-    <main>
-      <div className="contenedor__todo">
-        <div className="caja__trasera">
-          <div className="caja__trasera-login">
-            <h3>¿Ya tienes una cuenta?</h3>
-            <p>Inicia sesión para entrar en la página</p>
-            <button onClick={toggleLogin}>Iniciar Sesión</button>
-          </div>
-          <div className="caja__trasera-register">
-            <h3>¿Aún no tienes una cuenta?</h3>
-            <p>Regístrate para que puedas iniciar sesión</p>
-            <button onClick={toggleRegister}>Registrarse</button>
-          </div>
-        </div>
-
-        <div 
-          className="contenedor__login-register"
-          style={{ left: isLoginActive ? "0px" : "410px" }} // Ajustamos la posición
-        >
-          {isLoginActive ? (
-            <form className="formulario__login">
-              <h2>Iniciar Sesión</h2>
-              <input type="text" placeholder="Correo Electrónico" />
-              <input type="password" placeholder="Contraseña" />
-              <button>Entrar</button>
-            </form>
-          ) : (
-            <form className="formulario__register">
-              <h2>Registrarse</h2>
-              <input type="text" placeholder="Nombre completo" />
-              <input type="text" placeholder="Correo Electrónico" />
-              <input type="text" placeholder="Usuario" />
-              <input type="password" placeholder="Contraseña" />
-              <button>Registrarse</button>
-            </form>
-          )}
-        </div>
-      </div>
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Registro" element={<Registro />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
