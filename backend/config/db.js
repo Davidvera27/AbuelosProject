14f386af71338249b191b0f404a1f1328e66d88d
Davidvera27 <davidrestrepovera@gmail.com>
2024-10-22 21:25:31 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,20 @@
 // backend/config/db.js
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
+    // Conexión a la base de datos de MongoDB utilizando las variables de entorno
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useNewUrlParser: true, // Para evitar advertencias de Mongoose
+      useUnifiedTopology: true, // Para la correcta gestión de conexiones
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
+    // Manejo de errores de conexión y cierre del servidor si falla la conexión
     console.error(`Error: ${error.message}`);
-    process.exit(1); // Salir del proceso si falla la conexión
+    process.exit(1); // Cerrar el proceso si falla la conexión
   }
 };
+
 module.exports = connectDB;
