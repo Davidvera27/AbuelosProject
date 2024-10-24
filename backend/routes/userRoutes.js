0a3041cd03fbe23dc443a787e5653e8751bc1ea5
Davidvera27 <davidrestrepovera@gmail.com>
2024-10-23 21:50:59 -0500

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 // backend/routes/userRoutes.js
 const express = require('express');
-const { createUser, updateUser, getUserById } = require('../controllers/userController');
+const { createUser, loginUser, updateUser, getUserById } = require('../controllers/userController');
 
 const router = express.Router();
 
 // Ruta para crear un nuevo usuario (primer formulario)
 router.post('/users', createUser);
+
+// Ruta para iniciar sesión
+router.post('/users/login', loginUser);  // Corregido: loginUser debe estar importado
 
 // Ruta para actualizar el perfil del usuario (segundo formulario)
 router.put('/users/:id', updateUser);
